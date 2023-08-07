763efbdcb8641bdb7fe97584001efabca734fca5
Piotr Migaj <piotr.migaj@capgemini.com>
2023-08-07 14:43:13 +0200

    Initial commit

---
+++ src/test/java/cap/s42academy/AtmServerApplicationTest.java
@@ -0,0 +1,15 @@
+package cap.s42academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AtmServerApplicationTest {
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
