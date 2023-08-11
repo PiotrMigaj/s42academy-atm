5c89569ed2b24e69cd4452911e6f7c4d61154057
Piotr Migaj <piotr.migaj@capgemini.com>
2023-08-11 15:37:34 +0200

    test: add another test case to deposit money use case, to validate max number of transactions per day validator

--- src/test/java/cap/s42academy/account/application/service/DepositMoneyServiceIntegrationTest.java
+++ src/test/java/cap/s42academy/account/application/service/DepositMoneyServiceIntegrationTest.java
@@ -4,6 +4,7 @@ import cap.s42academy.IntegrationTestBase;
 import cap.s42academy.account.application.port.out.SaveAccountPort;
 import cap.s42academy.account.domain.entity.Account;
 import cap.s42academy.account.domain.valueobject.AccountStatus;
+import cap.s42academy.account.domain.valueobject.TransactionType;
 import cap.s42academy.printer.application.port.in.PrintReceiptUseCase;
 import cap.s42academy.user.domain.entity.User;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -154,6 +157,32 @@ class DepositMoneyServiceIntegrationTest extends IntegrationTestBase {
                 .andDo(log())
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.messages[0]").value(MAXIMUM_NUMBER_OF_TRANSACTIONS_PER_DAY_IS_EXCEEDED));
+    }
+
+    @Test
+    void shouldDepositMoney_whenLimitOfTransactionsIsReached_butTransactionStoredInDbIsMadeByAnotherUser() throws Exception {
+        //given
+        User user = existingUser(user());
+        existingSession(session(OPEN, LocalDateTime.now(),null,user));
+        Account account = existingAccount(account(user.getUserId().getValue(), AccountStatus.ACTIVE, new BigDecimal("120.00")));
+        existingTransaction(transaction(TransactionType.DEPOSIT,new BigDecimal("120.00"), LocalDate.now(), LocalTime.now(),account,false));
+        String creationRequest = """
+                {
+                  "accountId": "%s",
+                  "amount": "%s"
+                }
+                """.formatted(
+                account.getAccountId().getValue().toString(),
+                "120.00"
+        );
+        when(accountProperties.getMaxNumberOfTransactions()).thenReturn(1);
+        //when
+        //then
+        mockMvc.perform(post("/api/v1/accounts/deposit-money")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(creationRequest))
+                .andDo(log())
+                .andExpect(status().isOk());
     }
 
     @Test
--- src/test/java/cap/s42academy/user/application/service/RegisterUserServiceIntegrationTest.java
+++ src/test/java/cap/s42academy/user/application/service/RegisterUserServiceIntegrationTest.java
@@ -20,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 class RegisterUserServiceIntegrationTest extends IntegrationTestBase {
 
     @Autowired
